c04ad2784d80445523dfdc8e4e771d02fcf3b4bb
Lau Siaw Sam <lausiawsam98@gmail.com>
2022-10-13 21:03:18 +0800

    Add shell script to deploy k8s in one shot

---
+++ deploy-a2.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+echo "========Deploying kind cluster========\n"
+kind create cluster --name kind-1 --config k8s/kind/cluster-config.yaml
+echo "========Verify cluster status=========\n"
+docker ps
+kubectl cluster-info
+
+echo "========Load custom image into cluster nodes=========\n"
+kind load docker-image cs3219-otot-a1.3-app:latest --name kind-1
+echo "========Create k8s deployment=========\n"
+kubectl apply -f ./k8s/manifests/backend-deployment.yaml
+echo "========Create k8s service=========\n"
+kubectl apply -f ./k8s/manifests/backend-service.yaml
+echo "========Service status=========\n"
+kubectl get svc
+
+echo "========Create ingress controller=========\n"
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+echo "========Check nodes are ingress ready=========\n"
+kubectl get nodes -L ingress-ready
+echo "========Create ingress=========\n"
+kubectl apply -f ./k8s/manifests/backend-ingress.yaml
+echo "========Verify ingress=========\n"
+kubectl describe ingress
